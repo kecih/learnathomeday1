de16e9370b37c33bf6c759d961523a1af0418805
kecih <59724901+kecih@users.noreply.github.com>
2020-09-11 22:29:59 +0700

    add search profile element

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ const Post = ({ src }) => <img src={src} alt="profile picture" />;
 
 const Profile = (props) => {
   const { username } = props;
+  const [inputAccount, setInputAccount] = useState("");
   const [isFollowed, setIsFollowed] = useState(false);
-  const postCount = 0;
+  const postCount = 5;
   const posts = [...Array(postCount)];
 
   return (
     <>
+      <input
+        onChange={({ target: { value } }) => {
+          setInputAccount(value);
+        }}
+        placeholder="search profile"
+      />
+      <div>Search result for account : {inputAccount} </div>
       <img src="https://via.placeholder.com/150" alt="profile picture" />
       <div>@{username}</div>
       <button onClick={() => setIsFollowed(!isFollowed)}>
